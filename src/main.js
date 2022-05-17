4e519fbc04d5693438a2d300399982c0bbcdb054
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-17 23:42:18 +0900

    DOMの更新,削除,代入

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 const box1 = document.getElementById("box1");
 
 box1.style.color = "red";
+// test.style.color = "blue";
 
 box1.classList.add("mainBox");
+
+//DOMを更新する操作　setAttribute
+document.querySelector("#box1").setAttribute("id", "test");
+//DOM操作の削除
+// document.querySelector("#test").remove();
+//htmlの代入
+document.querySelector("#test").innerHTML = "<p>代入</p>";
