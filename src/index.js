6b2312335521dc3c121be8935dd44b1017a7d2d1
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-17 23:20:05 +0900

    getElementbyId

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./styles.css";
-
+import "./main";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
-<div >
+<div id="box1">
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
-<>
+<script src="main.js"></script>
+
 `;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+const box1 = document.getElementById("box1");
+
+box1.style.color = "red";
+
+box1.classList.add("mainBox");
