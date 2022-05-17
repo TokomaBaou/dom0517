5dcb726e385ab5cb32792bdb6fb0d1b8b6518e71
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-17 23:49:07 +0900

    appendChild

--- src/main.js
+++ src/main.js
@@ -15,3 +15,7 @@ document.querySelector("#test").innerHTML = "<p>代入</p>";
 let newItem = document.createElement("div");
 newItem.innerHTML = "Create Element";
 document.body.append(newItem);
+
+let newItem2 = document.createElement("div");
+newItem2.innerHTML = "append Child";
+document.querySelector("#test").appendChild(newItem2);
